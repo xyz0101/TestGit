a8f060fea71df1b7dc63f0f4f93bdced6732c189
sebrina <1394046585@qq.com>
2017-05-28 13:31:53 +0800

    13.31

--- TestGit/T.java
+++
@@ -1,7 +0,0 @@
-package com.tgit;
-
-public class T {
-public static void main(String[] args) {
-	System.out.println("2017/5/28");
-}
-}
--- TestGit/src/com/tgit/T.java
+++ TestGit/src/com/tgit/T.java
@@ -1,5 +1,7 @@
 package com.tgit;
 
 public class T {
-
+public static void main(String[] args) {
+	System.out.println("≤‚ ‘£ª13.30");
+}
 }
